ae1a8a2744fc3121b66f1e1775a2e4b38f9cb80d
Vishal <v.patel1093@gmail.com>
2017-05-23 23:59:09 -0400

    create post form and update controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,20 +1,23 @@
 class PostsController < ApplicationController
   def index
     @posts = current_user.posts
+    @post = current_user.posts.build
   end
 
   def create
     @post = current_user.posts.build(post_params)
     if @post.save
-      redirect_to posts_path, notice: "Post created"
+      flash[:success] = "Post created"
+      redirect_to posts_path
     else
-      redirect_to posts_path, alert: "Error posting"
+      flash[:alert] = "Error posting"
+      redirect_to posts_path
     end
   end
 
   private
 
   def post_params
-    params.require(:post).permit(:post_id, :content)
+    params.require(:post).permit(:content)
   end
 end

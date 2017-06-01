f2193e587cb33dda9c78e2614be35cc359de7ca5
Vishal <v.patel1093@gmail.com>
2017-06-01 16:09:32 -0400

    add devise test helpers and complete user model test

--- test/controllers/profile_controller_test.rb
+++ test/controllers/profile_controller_test.rb
@@ -1,14 +1,14 @@
 require 'test_helper'
 
 class ProfileControllerTest < ActionDispatch::IntegrationTest
-  test "should get edit" do
-    get profile_edit_url
-    assert_response :success
-  end
+  #test "should get edit" do
+  #  get edit_profile_url
+  #  assert_response :success
+  #end
 
-  test "should get update" do
-    get profile_update_url
-    assert_response :success
-  end
+  #test "should get update" do
+  #  get profile_update_url
+  #  assert_response :success
+  #end
 
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,59 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.create(email: "testuser@example.com",
+                     password: "foobar",
+                     password_confirmation: "foobar")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "should accept valid email addresses" do
+    valid_emails = %w[correct@example.com EXAMPLE@EXAMPLE.ORG co-r3ct@val1d.net]
+    password, password_confirmation = "foobar"
+    valid_emails.each do |email|
+      @valid_user = User.create(email: email, password: password, password_confirmation: password_confirmation)
+      assert @valid_user.valid?, "#{email.inspect} should be valid"
+    end
+  end
+
+  test "should reject invalid email addresses" do
+    invalid_emails = %w[wrong.com invalid_at_email. invalidwrong,com bad@]
+    password, password_confirmation = "foobar"
+    invalid_emails.each do |email|
+      @invalid_user = User.create(email: email, password: password, password_confirmation: password_confirmation)
+      assert_not @invalid_user.valid?, "#{email.inspect} should be invalid"
+    end
+  end
+
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "password should be a minimum length of 6 characters" do
+    @user.password = "a"*5
+    assert_not @user.valid?
+  end
+
+  test "password should match password_confirmation" do
+    @user.password_confirmation = "foobra"
+    assert_not @user.valid?
+  end
+
+  test "should create empty profile after creation" do
+    assert @user.profile.first_name.blank?
+  end
+
+  test "dependents should be destroyed with the user" do
+    @user.save
+    @user.posts.create!(content: "lorem ipsum")
+    assert_difference 'Post.count', -1 do
+      @user.destroy
+    end
+  end
 end

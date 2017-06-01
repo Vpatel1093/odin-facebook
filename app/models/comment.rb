28974ece18f47aca65c1e9c676a8bfa483871aed
Vishal <v.patel1093@gmail.com>
2017-06-01 18:00:03 -0400

    finish comment model test

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user
-  has_many :likes, as: :likeable
+  has_many :likes, as: :likeable, dependent: :destroy
 
-  validates_presence_of :user_id, :post, :content
+  validates_presence_of :user_id, :post_id, :content
 end
--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,37 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.create!(email: "testuser@example.com",
+                         password: "foobar",
+                         password_confirmation: "foobar")
+    @post = @user.posts.create(content: "lorem ipsum")
+    @comment = @post.comments.create(user: @user, content: "comment")
+  end
+
+  test "should be valid" do
+    assert @comment.valid?
+  end
+
+  test "should have content" do
+    @comment.content = ""
+    assert_not @comment.valid?
+  end
+
+  test "should have a user_id" do
+    @comment.user_id = nil
+    assert_not @comment.valid?
+  end
+
+  test "should have a post id" do
+    @comment.post_id = nil
+    assert_not @comment.valid?
+  end
+
+  test "dependent likes should be destroyed with the comment" do
+    @comment.likes.create!(user: @user)
+    assert_difference 'Like.count', -1 do
+      @comment.destroy
+    end
+  end
 end

e55b76fbfcf22ceb47a22e3ca6a06e99027b77f4
Vishal <v.patel1093@gmail.com>
2017-06-01 19:12:46 -0400

    finish friend request and user profile test

--- test/models/friend_request_test.rb
+++ test/models/friend_request_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class FriendRequestTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.create(email: "testuser@example.com",
+                     password: "foobar",
+                     password_confirmation: "foobar")
+    @user2 = User.create(email: "testuser2@example.com",
+                     password: "foobar",
+                     password_confirmation: "foobar")
+    @friend_request = FriendRequest.create(user: @user, friend: @user2)
+  end
+
+  test "should be valid" do
+    assert @friend_request.valid?
+  end
+
+  test "should not be accepted by default" do
+    assert_equal @friend_request.accepted, false
+  end
 end
--- test/models/profile_test.rb
+++ test/models/profile_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class ProfileTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.create(email: "testuser@example.com",
+                     password: "foobar",
+                     password_confirmation: "foobar")
+  end
+
+  test "should exist after user creation" do
+    assert @user.profile
+  end
+
+  test "first name and last name should be blank" do
+    assert @user.profile.first_name.blank? && @user.profile.last_name.blank?
+  end
 end

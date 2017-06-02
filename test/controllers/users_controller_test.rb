194e9bf05787e28784b2dcc2fb79ce68ab0e932f
Vishal <v.patel1093@gmail.com>
2017-06-02 15:31:46 -0400

    finish user controller test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:example1)
+    @another_user = users(:example2)
+  end
+
+  test "should be able to sign in" do
+    get new_user_session_url
+    sign_in @user
+  end
+
+  test "should redirect to login if not logged in" do
+    get users_path
+    assert_redirected_to new_user_session_url
+  end
+
+  test "should be able to access home page when signed in" do
+    sign_in @user
+    get users_path
+    assert_response :success
+  end
+
+  test "should not be able to destroy user if not signed in" do
+    assert_no_difference 'User.count' do
+      delete users_path(@user)
+    end
+  end
 end

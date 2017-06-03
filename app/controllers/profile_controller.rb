fed6c964467329377678408c002f7a0cadcfecae
Vishal <v.patel1093@gmail.com>
2017-06-03 19:14:39 -0400

    finish profiles controller tests

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -17,6 +17,6 @@ class ProfileController < ApplicationController
   private
 
     def profile_params
-      params.require[:profile].permit[:first_name, :last_name]
+      params.require(:profile).permit(:first_name, :last_name)
     end
 end
--- test/controllers/profile_controller_test.rb
+++ test/controllers/profile_controller_test.rb
@@ -1,14 +1,25 @@
 require 'test_helper'
 
 class ProfileControllerTest < ActionDispatch::IntegrationTest
-  #test "should get edit" do
-  #  get edit_profile_url
-  #  assert_response :success
-  #end
+  def setup
+    @user = users(:example1)
+    @user_profile = profiles(:example1)
+  end
 
-  #test "should get update" do
-  #  get profile_update_url
-  #  assert_response :success
-  #end
+  test "should get redirect if not signed in" do
+    get edit_profile_url(@user_profile)
+    assert_redirected_to new_user_session_url
+  end
 
+  test "should get edit" do
+    sign_in @user
+    get edit_profile_url(@user_profile)
+    assert_response :success
+  end
+
+  test "should get update" do
+    sign_in @user
+    patch profile_url(@user_profile), params: { profile: { first_name: "example", last_name: "user" } }
+    assert_redirected_to user_path(@user)
+  end
 end

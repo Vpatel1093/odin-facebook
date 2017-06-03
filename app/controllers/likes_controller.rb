6510b4b68894ac51fa565d6157c743befe5f0ab5
Vishal <v.patel1093@gmail.com>
2017-06-03 18:29:21 -0400

    finish likes controller tests

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,4 +1,6 @@
 class LikesController < ApplicationController
+  before_action :friend_of_likeable_creator?, only: :create
+
   def create
     @likeable = find_likeable
     @like = current_user.likes.build(likeable: @likeable)
@@ -15,7 +17,7 @@ class LikesController < ApplicationController
     @like = Like.find_by(likeable_id: @likeable.id, user_id: current_user.id)
     @like.destroy
     flash[:notice] = "Unliked."
-    redirect_to :back
+    redirect_to posts_path
   end
 
   private
@@ -27,5 +29,14 @@ class LikesController < ApplicationController
         end
       end
       nil
+    end
+
+    def friend_of_likeable_creator?
+      @likeable = find_likeable
+      likeable_creator = User.find_by(id: @likeable.user_id) if @likeable
+      friendships = FriendRequest.where(user_id: likeable_creator.id).where(friend_id: current_user.id).where(accepted: true) + FriendRequest.where(user_id: current_user.id).where(friend_id: likeable_creator.id).where(accepted: true) if likeable_creator
+      not_friends = friendships.empty? if likeable_creator
+      current_user_is_likeable_creator = current_user.id == likeable_creator.id if likeable_creator
+      redirect_to posts_path if not_friends && !(current_user_is_likeable_creator)
     end
 end
--- test/controllers/likes_controller_test.rb
+++ test/controllers/likes_controller_test.rb
@@ -1,7 +1,50 @@
 require 'test_helper'
 
 class LikesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:example1)
+    @user_post = posts(:post1)
+    @user_like_of_own_post = likes(:like1)
+    @another_users_post = posts(:post2)
+    @another_users_comment = comments(:comment2)
+    @friend_of_users_post = posts(:post3)
+  end
+
+  test "should redirect comment like unless signed in" do
+    post comment_likes_path(@another_users_comment)
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should redirect post like unless signed in" do
+    post post_likes_path(@another_users_post)
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should be able to like own post" do
+    sign_in @user
+    post post_likes_path(@user_post)
+    assert :success
+  end
+
+  test "should not be able to like likeable of nonfriend" do
+    sign_in @user
+    assert_no_difference 'Like.count' do
+      post comment_likes_path(@another_users_comment)
+    end
+    assert_redirected_to posts_path
+  end
+
+  test "should be able to like likeable of friend" do
+    sign_in @user
+    assert_difference 'Like.count' do
+      post post_likes_path(@friend_of_users_post)
+    end
+  end
+
+  test "should be able to unlike" do
+    sign_in @user
+    assert_difference 'Like.count', -1 do
+      delete post_like_path(@user_post, @user_like_of_own_post)
+    end
+  end
 end

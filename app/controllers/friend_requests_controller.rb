8149b1b72d15e53e77a4922f4e3a7e9e8445d0b7
Vishal <v.patel1093@gmail.com>
2017-06-04 13:29:10 -0400

    finish friend requests controller tests

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -4,19 +4,19 @@ class FriendRequestsController < ApplicationController
   end
 
   def create
-    @friend_request = current_user.friend_request.build(friend_id: params[:friend_id])
+    @friend_request = current_user.friend_requests.build(friend_id: params[:friend_id])
     if @friend_request.save
       flash[:notice] = "Friend requested."
-      redirect_to :back
+      redirect_to users_url
     else
       flash[:error] = "Unable to request friendship."
-      redirect_to :back
+      redirect_to users_url
     end
   end
 
   def update
     @friend_request = FriendRequest.find_by(id: params[:id])
-    @friend_request.update(status: "accepted")
+    @friend_request.update(accepted: true)
     if @friend_request.save
       flash[:notice] = "Friend request accepted."
       redirect_to root_url
@@ -30,6 +30,6 @@ class FriendRequestsController < ApplicationController
     @friend_request = FriendRequest.find_by(id: params[:id])
     @friend_request.destroy
     flash[:notice] = "Friend request denied."
-    redirect_to :back
+    redirect_to root_url
   end
 end
--- test/controllers/friend_requests_controller_test.rb
+++ test/controllers/friend_requests_controller_test.rb
@@ -1,19 +1,33 @@
 require 'test_helper'
 
 class FriendRequestsControllerTest < ActionDispatch::IntegrationTest
-  #test "should get create" do
-  #  get friend_requests_create_url
-  #  assert_response :success
-  #end
+  def setup
+    @user = users(:example1)
+    @friendship = friend_requests(:one)
+    @another_user = users(:example2)
+    @pending_friend_request = friend_requests(:two)
+  end
 
-  #test "should get update" do
-  #  get friend_requests_update_url
-  #  assert_response :success
-  #end
+  test "should get redirect if not signed in" do
+    get friend_requests_url
+    assert_redirected_to new_user_session_path
+  end
 
-  #test "should get destroy" do
-  #  get friend_requests_destroy_url
-  #  assert_response :success
-  #end
+  test "should get create" do
+    sign_in @user
+    post friend_requests_url(@another_user)
+    assert_redirected_to users_url
+  end
 
+  test "should get update" do
+    sign_in @user
+    patch friend_request_url(@pending_friend_request)
+    assert_redirected_to root_url
+  end
+
+  test "should get destroy" do
+    sign_in @user
+    delete friend_request_url(@pending_friend_request)
+    assert_redirected_to root_url
+  end
 end
